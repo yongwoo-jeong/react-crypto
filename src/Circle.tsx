e0a2bffe7c64bb2d56265eed4681179505d071a7
yongwoo-jeong <jywoo9@gmail.com>
2022-01-09 23:10:53 +0900

    props, interface

---
+++ src/Circle.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+  bgColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+`;
+
+interface CircleProps {
+  bgColor: string;
+}
+
+function Circle({ bgColor }: CircleProps) {
+  return <Container bgColor={bgColor} />;
+}
+
+export default Circle;
+
+interface PlayerShape {
+  name: string;
+  age: number;
+}
+
+const sayHello = (playerObj: PlayerShape) => `Hello ${playerObj.name}`;
+
+sayHello({ name: "nico", age: 13 });
