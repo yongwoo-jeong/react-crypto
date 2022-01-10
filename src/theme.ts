11950fb0bcaa21f47c12c6c4de2367b34171fb70
yongwoo-jeong <jywoo9@gmail.com>
2022-01-10 17:50:41 +0900

    crypto tracker Home

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
-export const lightTheme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
-};
-
-export const darkTheme: DefaultTheme = {
-  bgColor: "black",
-  textColor: "white",
-  btnColor: "teal",
+export const theme: DefaultTheme = {
+  bgColor: "#2d3436",
+  textColor: "#dfe6e9",
+  accentColor: "#9c88ff",
 };
