ea3552665cf4bc63f51306a3e8ee7ecaffc66921
yongwoo-jeong <jywoo9@gmail.com>
2022-01-13 11:32:19 +0900

    before use react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient } from "react-query";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { theme } from "./theme";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
